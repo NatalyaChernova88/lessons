96303455327e496a4728da4f480d258cf377a5e9
NatalyaChernova88 <navichernova@gmail.com>
2024-10-26 19:36:48 +0400

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,17 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
-while index < len(my_list) and my_list[index] >= 0:
-    print(my_list[index])
-    index += 1
-    continue
+n = my_list[index]
+while n >= 0 and index < len(my_list):
+    n = my_list[index]
+    index = index + 1
+    if n < 0:
+        break
+    elif n == 0:
+        continue
+    else:
+        print(n)
+
+
+
+
+
